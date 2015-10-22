bbc9df95f68763b372e8472effad33bbab6ce262
lukasz.harasimowicz <developer@hardev.pl>
2015-10-23 00:06:42 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='mockingbird',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/harnash/mockingbird',
+    license='MIT',
+    author='£ukasz Harasimowicz',
+    author_email='lukasz@harnash.eu',
+    description='',
+    install_requires=[
+        # 'flask==1.0.0', for now just install from github
+        'nose==1.3.7',
+        'Flask-Restless==0.17.0',
+    ]
+)
